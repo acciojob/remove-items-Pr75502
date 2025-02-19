29f50a4744315c8bbb2274881480ad3f46444ecb
Pr75502 <pp1614582@gmail.com>
2025-02-19 15:00:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required
+let color=document.querySelector("#colorSelect")
+let remove=document.querySelector('input[type="button"]')
+remove.addEventListener("click", ()=>{
+	let selectedIndex=color.selectedIndex;
+	if(selectedIndex != -1){
+	color.remove(selectedIndex)
+	}
+
+})
